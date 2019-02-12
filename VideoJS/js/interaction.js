72377ba588b92ba1a8e9e23b430dec4ad5fe23d6
darshangit <darshans1989@gmail.com>
2019-02-12 11:49:03 +0530

    Videojs Markers

--- VideoJS/js/interaction.js
+++ VideoJS/js/interaction.js
@@ -7,7 +7,11 @@ var currentTime;
 var halfwayPoint;
 var durationTime;
 
+var video1;
+
 videojs('video1').ready(function(){
+
+	video1 = videojs('video1');
 
 	this.on('loadedmetadata', function(){
 		alert(this.duration())
@@ -15,11 +19,30 @@ videojs('video1').ready(function(){
 	this.on('timeupdate', function(){
 		console.log(this.currentTime())
 	})
+
+	video1.markers({
+		markerStyle: {
+			'border-radius': '20px',
+			'background-color': 'blue'
+		},
+		markers: [
+			{time: 5, text: "First Marker"},
+			{time: 10, text: "Second Marker"}
+		],
+		onMarkerClick: function(marker){
+			alert(marker.text)
+		},
+		onMarkerReached: function(marker){
+			if(marker.text == 'First Marker'){
+				$("#modal1").modal('show');
+			}
+		}
+	})
 })
 
 // Animate in menu
 function animateMenu(){
-	videojs('video1').play();
+	// video1.play();
 	// if (menuIn) {
 	// 	$('.menu').animate({right: "0"}, 500);
 	// 	menuIn = false;
@@ -34,12 +57,38 @@ function jumpToPoint(time){
 	
 }
 
+function updateTime(time){
+	video1.currentTime(time)
+	video1.play();
+}
+
+function muteAudio(){
+	var isVloumeMuted = video1.muted();
+
+	if(isVloumeMuted){
+		video1.muted(false)
+	}else{
+		video1.muted(true)
+	}
+}
+
+function togglePlay(){
+	var isPaused = video1.paused();
+	var remainingTime = video1.remainingTime();
+	if(isPaused){
+		video1.play()
+		video1.requestFullscreen();
+	}else{
+		video1.pause();
+	}
+}
+
 // Modal Show
 $(document).on('show.bs.modal','#modal1', function () {
-	// video1.pause();
+	video1.pause();
 });
 
 // Modal Hide
 $(document).on('hide.bs.modal','#modal1', function () {
-	// video1.play();
+	video1.play();
 });
